04f359fe1d88e996c5476367f0be3f26328c81f5
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-05-20 18:34:42 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "tags" {
+  description = "A map of tags to add to resources"
+  type        = map(string)
+  default     = {
+    Environment = "development"
+    Project     = "example-project"
+  }
+}
