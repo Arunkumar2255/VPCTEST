29ffcfdcb49fa840a7d29309636cc1503b2a204e
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-05-20 18:35:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,90 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = merge({
+    Name = "main-vpc"
+  }, var.tags)
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  tags = merge({
+    Name = "public-subnet-${count.index}"
+  }, var.tags)
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  tags = merge({
+    Name = "private-subnet-${count.index}"
+  }, var.tags)
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = merge({
+    Name = "main-igw"
+  }, var.tags)
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = merge({
+    Name = "public-rt"
+  }, var.tags)
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" {
+  count = length(aws_subnet.private)
+  vpc = true
+}
+
+resource "aws_nat_gateway" "main" {
+  count = length(aws_subnet.private)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id = element(aws_subnet.public.*.id, count.index)
+  tags = merge({
+    Name = "main-nat-${count.index}"
+  }, var.tags)
+}
+
+resource "aws_route_table" "private" {
+  count = length(aws_subnet.private)
+  vpc_id = aws_vpc.main.id
+  tags = merge({
+    Name = "private-rt-${count.index}"
+  }, var.tags)
+}
+
+resource "aws_route" "private_nat_gateway" {
+  count = length(aws_subnet.private)
+  route_table_id = aws_route_table.private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main[count.index].id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private)
+  subnet_id = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
